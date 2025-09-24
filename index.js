4176f0b96b564e1ad1d3d5c35b11d877f6aadbdf
Elly Omondi <owuorelly3@gmail.com>
2025-09-24 19:13:25 +0300

    Main

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your code here!
+// Remove the existing <main id="main">
+const main = document.getElementById("main");
+main.remove();
+
+// Create a new <h1> and store it in the variable newHeader
+const newHeader = document.createElement("h1");
+
+// Give it the id 'victory'
+newHeader.id = "victory";
+
+// Set the text content to include your name
+newHeader.textContent = "Elly Owuor is the champion";
+
+// Finally, add the new header to the document body
+document.body.appendChild(newHeader);
